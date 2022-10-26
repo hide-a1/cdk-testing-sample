ee60768caabca68b1318cd0c0998ac682957827d
Hideki Arima <hide210g@gmail.com>
2022-10-26 16:44:14 +0900

    init

---
+++ src/app/counter/counter-harness.ts
@@ -0,0 +1,28 @@
+// couter-harness.ts
+
+import { ComponentHarness } from '@angular/cdk/testing';
+import { MatButtonHarness } from '@angular/material/button/testing';
+
+export class CounterHarness extends ComponentHarness {
+  // html内で対象にするタグを指定
+  static hostSelector = 'app-counter';
+
+  // locatorFor = 要素を特定するための関数
+  private getDisplayCountLocator = this.locatorFor('.count-display');
+
+  private getIncrementLocator = this.locatorFor(
+    MatButtonHarness.with({
+      text: 'Increment',
+    })
+  );
+
+  async getDisplayedCount(): Promise<string> {
+    const display = await this.getDisplayCountLocator();
+    return await display.text();
+  }
+
+  async increment(): Promise<void> {
+    const button = await this.getIncrementLocator();
+    await button.click();
+  }
+}
---
+++ src/app/counter/counter.component.spec.ts
@@ -0,0 +1,61 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { CounterComponent } from './counter.component';
+import { By } from '@angular/platform-browser';
+import { CounterHarness } from './counter-harness';
+
+describe('CounterComponent', () => {
+  let component: CounterComponent;
+  let fixture: ComponentFixture<CounterComponent>;
+  let harness: CounterHarness;
+
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      declarations: [CounterComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(async () => {
+    fixture = TestBed.createComponent(CounterComponent);
+    component = fixture.componentInstance;
+    harness = await TestbedHarnessEnvironment.harnessForFixture(
+      fixture,
+      CounterHarness
+    );
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    // OnInitまでは安全
+    expect(component).toBeTruthy();
+  });
+
+  it('should display count', async () => {
+    component.count = 100;
+
+    // ハーネスなし
+    fixture.detectChanges();
+    const displayCount = fixture.debugElement.query(By.css('.count-display'));
+    expect(displayCount.nativeElement.textContent).toBe('100');
+
+    // ハーネス有り
+    // const displayCount = await harness.getDisplayedCount();
+    // expect(displayCount).toBe('100');
+  });
+
+  it('should emit increment', async () => {
+    spyOn(component.increment, 'emit');
+
+    // ハーネス無し
+    const button = fixture.debugElement.queryAll(By.css('button'))[0];
+    (button.nativeElement as HTMLButtonElement).click();
+    fixture.detectChanges();
+
+    expect(component.increment.emit).toHaveBeenCalled();
+
+    // ハーネス有り
+    // await harness.increment();
+
+    // expect(component.increment.emit).toHaveBeenCalled();
+  });
+});
